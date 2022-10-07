9393d8743517e2a091a69a69e9b82b0a07d3b705
Mikhail Tovt <mikhail.tovt.02@mail.ru>
2022-10-07 20:48:22 +0300

    charts

--- src/Pages/CurrenciesList/CurrencyItemPage.js
+++ src/Pages/CurrenciesList/CurrencyItemPage.js
@@ -1,29 +1,51 @@
 import React, {useEffect, useState} from 'react';
+import { MyLine } from './Graph.tsx'
 import { useParams } from 'react-router-dom'
 import { LAST_MONTH_CURRENCIES } from '../../constanse/LastMonthCurrencies'
 
 const CurrencyItemPage = (props) => {
 
     const [currentCurrency, setCurrentCurrency] = useState([])
+    const [data, setData] = useState({})
     const currencyName = useParams().id
     const params = useParams()
 
     useEffect(() => {
         const currentCurrency = { name: params.id, data: LAST_MONTH_CURRENCIES[params.id] }
         setCurrentCurrency(currentCurrency)
+        const labels = currentCurrency.data.map((cc) => {
+            return cc["<DATE>"]
+        });
+
+        const data = {
+            labels,
+            datasets: [
+                {
+                    label: currencyName,
+                    data: currentCurrency.data.map((cc) => cc["<HIGH>"]),
+                    borderColor: 'rgb(255, 99, 132)',
+                    backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                }
+            ],
+        }
+        console.log(currentCurrency)
+        setData(data)
     }, [params.id])
 
     const CurrencyName = () => {
-        return ( 
+        return (
             currencyName
         )
     }
 
     const CurrencyDynamic = () => {
         if (currentCurrency?.data) {
-            return ( 
+            return (
                 <div className='col-md-8 mx-auto text-center mt-5'>
                     <ul className='list-group'>
+                        <MyLine
+                            data={data}
+                        />
                         {currentCurrency.data.map((currency) => {
                             return <li className='list-group-item' key={currency["<DATE>"]}> {currency["<DATE>"]} - {currency["<HIGH>"]} </li>
                         })}
@@ -32,7 +54,7 @@ const CurrencyItemPage = (props) => {
             )
         }
         else {
-            return ( 
+            return (
                 <div className='col-md-8 mx-auto text-center mt-5'>
                     Нет данных
                 </div>
@@ -41,9 +63,9 @@ const CurrencyItemPage = (props) => {
     }
 
     return (
-        <div className="container-fluid"> 
+        <div className="container-fluid">
             <div className="row">
-                <div className='col-md-6 mx-auto text-center'>        
+                <div className='col-md-6 mx-auto text-center'>
                     <CurrencyName />
                 </div>
             </div>
---
+++ src/Pages/CurrenciesList/Graph.tsx
@@ -0,0 +1,38 @@
+// @ts-ignore
+import React from 'react';
+
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js'
+import { Line } from 'react-chartjs-2'
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+export const options = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'top' as const,
+        }
+    },
+};
+
+export function MyLine(props) {
+
+    return <Line options={options} data={props.data} />;
+}
